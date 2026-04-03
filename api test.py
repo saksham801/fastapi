3bae89d0b8a1a279579d91c44ce4f939fed25f80
Saksham Dubey <dubeysaksham801@gmail.com>
2026-04-03 14:20:18 +0530

    commit added

---
+++ api test.py
@@ -0,0 +1,14 @@
+import requests
+
+API_KEY = "sk-saksham-local-123"
+
+headers = {
+    "Authorization": f"Bearer {API_KEY}"
+}
+
+res = requests.get(
+    "http://localhost:3001/api/v1/models",
+    headers=headers
+)
+
+print(res.text)
--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = FastAPI()
 async def root():
     return {"message": "HEllo Claude"}
 
+@app.get("/login")
+async def login():
+    return {"message": "Hello login"}
+
 # This is the ONLY way Cloudflare knows how to talk to FastAPI
 async def on_fetch(request, env):
     return await app(request, env)
